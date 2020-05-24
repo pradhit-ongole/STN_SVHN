38a5bd675a37c0678dde3ef6d835185ee9fcf8a9
Pradhit Ongole <pradhitongole@gmail.com>
2020-05-24 23:46:06 +0530

    preprocessing

--- preprocessSVHN.py
+++ preprocessSVHN.py
@@ -8,8 +8,8 @@ def preprocess(x_train, x_test, y_train, y_test):
     y_train[y_train==10]=0
     y_test[y_test==10]=0
 
-    x_train = x_train[:,:,7:24,:]
-    x_test = x_test[:,:,7:24,:]
+    x_train = x_train[:,:,10:21,:]
+    x_test = x_test[:,:,10:21,:]
 
     x_train = x_train/255
     x_test = x_test/255
